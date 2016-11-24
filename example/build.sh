543a869c8c6d8c7d205d0c55835f7be9a314b420
Michael Garski <mgarski@fender.com>
2016-11-24 10:55:59 -0800

    update sample to use .env

--- example/build.sh
+++ example/build.sh
@@ -5,25 +5,6 @@ set -x
 
 GOOS=linux go build -o main
 
-zip -r lambda.zip main index.js
+zip -r lambda.zip main index.js .env
 
 # upload lambda.zip as lambda function
-# echoes back values received as input on event object
-
-# Sample event data:
-# {
-#   "key3": "value3",
-#   "key2": "value2",
-#   "key1": "value1"
-# }
-
-# Lambda Proc output:
-# {
-#   "proc_req_id": 0,
-#   "error": null,
-#   "data": {
-#     "key1": "value1",
-#     "key2": "value2",
-#     "key3": "value3"
-#   }
-# }
--- example/main.go
+++ example/main.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"os"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/fenderdigital/lambda_proc"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	logrus.SetOutput(os.Stderr)
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Warn("No .env file to load")
+	}
 
 	lambda_proc.Run(func(context *lambda_proc.Context, eventJSON json.RawMessage) (interface{}, error) {
-
 		var v map[string]interface{}
 		if err := json.Unmarshal(eventJSON, &v); err != nil {
 			return nil, err
 		}
+		logrus.Infof("env var: %s", os.Getenv("KEY"))
+		logrus.Infof("context: %+v", *context)
+		data, _ := json.MarshalIndent(&v, "", "    ")
+
+		logrus.Infof("event: %+v", string(data))
 		return v, nil
-
 	})
-
 }
